f98d445cbbcd2c6c2d70f529442414b56d381239
Diego Pulido <diego.pulido@ariadnacg.com>
2021-12-02 14:19:01 -0500

    12/2

--- main.js
+++ main.js
@@ -18,9 +18,14 @@ function getAll(){
             logo.innerHTML = fotosGaleria[i]['logo'];
             let category = document.createElement('h5');
             category.innerHTML = fotosGaleria[i]['category'];
-            card.appendChild(logo);
-            card.appendChild(category);
-
+            let separator = document.createElement('div');
+            separator.classList.add('separator');
+            let innerCard = document.createElement('div');
+            innerCard.classList.add('innerCard');
+            innerCard.appendChild(logo);
+            innerCard.appendChild(separator);
+            innerCard.appendChild(category);
+            card.appendChild(innerCard);
             let gallery = document.getElementsByClassName('gallery')[0];
             gallery.appendChild(card);
         };
@@ -30,7 +35,7 @@ function getAll(){
     .catch(function(error) {
         console.log('Error' + error.message);
     });
-}
+};
 
 
 
@@ -43,10 +48,59 @@ function cardBlock(){
     for (i = 0; i < cards.length; i++){
         cards[i].classList.add("cardBlock");
     };
-}
+};
 
 function cardRow(){
     for (i = 0; i < cards.length; i++){
         cards[i].classList.remove("cardBlock");
+    };
+};
+
+
+
+
+function catBranding() {
+    for (i = 0; i < cards.length; i++){
+        if ((cards[i].classList.contains('cardBranding')) == true){
+            cards[i].style.display = "block";
+        } else {
+            cards[i].style.display = "none";
+        }
+    };
+}
+
+function catWeb() {
+    for (i = 0; i < cards.length; i++){
+        if ((cards[i].classList.contains('cardWeb')) == true){
+            cards[i].style.display = "block";
+        } else {
+            cards[i].style.display = "none";
+        }
+    };
+}
+
+function catPhoto() {
+    for (i = 0; i < cards.length; i++){
+        if ((cards[i].classList.contains('cardPhotography')) == true){
+            cards[i].style.display = "block";
+        } else {
+            cards[i].style.display = "none";
+        }
+    };
+}
+
+function catApp() {
+    for (i = 0; i < cards.length; i++){
+        if ((cards[i].classList.contains('cardApp')) == true){
+            cards[i].style.display = "block";
+        } else {
+            cards[i].style.display = "none";
+        }
+    };
+}
+
+function catAll() {
+    for (i = 0; i < cards.length; i++){
+        cards[i].style.display = "block";  
     };
 }
